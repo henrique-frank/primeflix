f64733009f59997470f3685fc133a7eb208b231e
henrique-frank <henrique_frank@hotmail.com>
2024-02-05 13:17:19 -0300

    Listing movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,56 @@
+import { useEffect, useState} from 'react';
+import api from '../../services/api'
+
+// https://api.themoviedb.org/3/movie/now_playing?api_key={{api_key}}&language=pt-BR
+
+
+// fetch('../pages/Home/api-key.txt')
+// .then(function(response) {
+//     console.log(response);
+//   return response.text();
+// })
+// .then(function(data) {
+//   var apiKey = data.trim();
+//   console.log(apiKey);
+// })
+// .catch(function(error) {
+//   console.error('Erro ao carregar a chave de API.', error);
+// });
+
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: apikey,
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            setFilmes(response.data.results.slice(0,10));
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return(
-        <div>
-            <h1>BEM VINDO A HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=> {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path} alth={filme.title`} />
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 // Base da URL: https://api.themoviedb.org/3/
 // URL DA API: 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+// https://api.themoviedb.org/3/movie/now_playing
+
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default api;
+
